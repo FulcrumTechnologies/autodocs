1484d63636150bbe175af28e00481f187effaaab
calebh93 <hawkins.caleb93@gmail.com>
2016-03-10 11:44:47 -0800

    Changes to write_vms to comply with Skytap script

--- write_vms.py
+++ write_vms.py
@@ -10,19 +10,22 @@ import json
 import write_page
 
 
-def create_one(env_details, parent_id, parent_name):
+def create_one(v, parent_id, parent_name):
     """Just make one vm page; primarily called by create(), seen below."""
 
-    print (env_details.name + ": Writing content..."),
+    print (v.name + ": Writing content..."),
 
     # Making a json containing important information. This will be stored in
     # a file in JSONS directory and used to perform various functions
     # related to Wiki Keeper.
     json_info = {}
 
-    vm_name = env_details.name
-    vm_hostname = env_details.interfaces[0].hostname
-    vm_id = env_details.id
+    vm_name = v.name
+
+    for i in v.interfaces:
+        vm_hostname = i.hostname
+
+    vm_id = str(v.id)
 
     json_info["vm_name"] = vm_name
     json_info["vm_hostname"] = vm_hostname
@@ -30,68 +33,86 @@ def create_one(env_details, parent_id, parent_name):
     json_info["parent_page_name"] = parent_name
 
     content = "<p>"
+    new_content = ""
 
     content += ("ID: " + vm_id + "<br/><br/>")
     json_info["id"] = vm_id
 
-    content += ("Configuration URL: " + env_details.configuration_url + "<br/>")
-    json_info["config_url"] = env_details.configuration_url
+    content += ("Configuration URL: " + v.configuration_url + "<br/>")
+    json_info["config_url"] = v.configuration_url
 
     json_info["nat_ip_us"] = ""
     json_info["nat_ip_india"] = ""
 
-    try:
-        content += ("<br/>")
-        for k in env_details.interfaces[0].nat_addresses.vpn_nat_addresses:
-            if (k.vpn_id == "vpn-3631944" or
-                    k.vpn_id == "vpn-661182"):
-                content += ("NAT IP (US): " + k.ip_address + "<br/>")
-                json_info["nat_ip_us"] = k.ip_address
-            elif k.vpn_id == "vpn-3288770":
-                content += ("NAT IP (India): " + k.ip_address + "<br/>")
-                json_info["nat_ip_india"] = k.ip_address
-    except (KeyError, IndexError):
-        content += ("NAT IP (US): " + env_details.interfaces[0].ip + "<br/>")
-        json_info["nat_ip_us"] = env_details.interfaces[0].ip
-        json_info["nat_ip_india"] = ""
+    # try:
+    #     content += ("<br/>")
+    #     for k in e.interfaces[0].nat_addresses.vpn_nat_addresses:
+    #         if (k.vpn_id == "vpn-3631944" or
+    #                 k.vpn_id == "vpn-661182"):
+    #             content += ("NAT IP (US): " + k.ip_address + "<br/>")
+    #             json_info["nat_ip_us"] = k.ip_address
+    #         elif k.vpn_id == "vpn-3288770":
+    #             content += ("NAT IP (India): " + k.ip_address + "<br/>")
+    #             json_info["nat_ip_india"] = k.ip_address
+    # except (KeyError, AttributeError):
+    #     content += ("NAT IP (US): " + e.interfaces[0].ip + "<br/>")
+
+    for i in v.interfaces:
+        json_info["nat_ip_us"] = i.ip
+
+    json_info["nat_ip_india"] = ""
 
     json_info["vpn_id_us"] = ""
     json_info["vpn_id_india"] = ""
 
-    try:
-        for k in env_details.interfaces[0]nat_addresses.vpn_nat_addresses:
-            if k.vpn_name.startswith("ASAOPS"):
-                content += ("VPN ID (US): " + k.vpn_id + "<br/>")
-                json_info["vpn_id_us"] = k.vpn_id
-            elif k["vpn_name"].startswith("ASASG"):
-                content += ("VPN ID (India): " + k.vpn_id + "<br/>")
-                json_info["vpn_id_india"] = k.vpn_id
-    except (KeyError, IndexError):
-        # Can't find "nat_addresses"
-        pass
-
-        content += ("<br/>")
-
-    try:
-        content += ("External IP: " + env_details.interfaces[0].services[0].external_ip + "<br/>")
-        json_info["external_ip"] = env_details.interfaces[0].services[0].external_ip
-    except IndexError:
-        pass
+    # try:
+    #     for k in e.interfaces[0]nat_addresses.vpn_nat_addresses:
+    #         if k.vpn_name.startswith("ASAOPS"):
+    #             content += ("VPN ID (US): " + k.vpn_id + "<br/>")
+    #             json_info["vpn_id_us"] = k.vpn_id
+    #         elif k["vpn_name"].startswith("ASASG"):
+    #             content += ("VPN ID (India): " + k.vpn_id + "<br/>")
+    #             json_info["vpn_id_india"] = k.vpn_id
+    # except (KeyError, IndexError):
+    #     # Can't find "nat_addresses"
+    #     pass
+    #
+    #     content += ("<br/>")
 
     try:
-        content += ("External port: " + str(env_details.interfaces[0].services[0].external_port) + "<br/>")
-        json_info["external_port"] = str(env_details.interfaces[0].services[0].external_port)
-    except IndexError:
+        for i in v.interfaces:
+            for s in i.services:
+                new_content = ("External IP: " + i.s.external_ip + "<br/>")
+                json_info["external_ip"] = i.s.external_ip
+    except AttributeError:
+        pass
+
+    content += new_content
+    new_content = ""
+
+    try:
+        for i in v.interfaces:
+            for s in i.services:
+                new_content = ("External port: " + str(i.s.external_port) + "<br/>")
+                json_info["external_port"] = str(i.s.external_port)
+    except AttributeError:
         pass
 
     try:
-        content += ("Internal port: " + str(env_details.interfaces[0].services[0].internal_port) + "<br/><br/>")
-        json_info["internal_port"] = str(env_details.interfaces[0].services[0].internal_port)
-    except IndexError:
+        content += ("Internal port: " + str(e.interfaces[0].services[0].internal_port) + "<br/><br/>")
+        json_info["internal_port"] = str(e.interfaces[0].services[0].internal_port)
+    except AttributeError:
         pass
 
-    content += ("Local IP: " + env_details.interfaces[0].ip + "<br/>")
-    json_info["local_ip"] = env_details.interfaces[0].ip
+    content += new_content
+    new_content = ""
+
+    for i in v.interfaces:
+        new_content = ("Local IP: " + i.ip + "<br/>")
+        json_info["local_ip"] = i.ip
+
+    content += new_content
+    new_content = ""
 
     content += ("<br/><br/>")
 
@@ -107,10 +128,10 @@ def create_one(env_details, parent_id, parent_name):
             json.dump(json_info, file)
 
 
-def create(data, parent_id, parent_name):
+def create(e, parent_id, parent_name):
     """Create vm wiki page(s)."""
 
     print ("Writing pages for vms.")
 
-    for i in data["vms"]:
-        create_one(i, parent_id, parent_name)
+    for v in e.vms:
+        create_one(v, parent_id, parent_name)
