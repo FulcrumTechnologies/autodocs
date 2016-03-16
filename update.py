8546256f5f50b15b994b72a8814687afdc502a7a
calebh93 <chawkins@fulcrum.net>
2016-03-16 13:12:36 -0700

    Some initial work to get the ball rolling.

---
+++ update.py
@@ -0,0 +1,21 @@
+import skytap
+import update_write
+
+def start(args):
+    """Redirect to update function based on args."""
+
+    envs = skytap.Environments()
+
+    if (args[1] == "write"):
+        os.system("clear")
+        print ("Writing wiki pages.")
+        update_write.start(envs)
+    else:
+        print ("Command not recognized.")
+
+    print ("\n\nJob\'s done.\n")
+
+
+if __name__ == '__main__':
+    start(sys.argv)
+
---
+++ update_write.py
@@ -0,0 +1,19 @@
+"""Write wiki pages of environments which currently have none.
+
+"""
+
+import create_page
+import json
+
+
+def start(envs):
+    """Write all remaining pages of environments not currently listed."""
+
+    env_all = 0
+
+    # Count up total so we have completetion dialogue (1/95, 2/95, etc.)
+    for e in envs:
+        env_all += 1
+
+    print ("Total environments: " + str(env_all))
+
